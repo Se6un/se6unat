19f0842b2cf3b441962b5f736f099d145bd14ebc
Se6un <104276466+Se6un@users.noreply.github.com>
2024-02-28 05:21:54 +0300

    11
    111

--- Rule34/gotopage.js
+++ Rule34/gotopage.js
@@ -1,3 +1,9 @@
+// Отображение первой страницы при загрузке сайта
+window.addEventListener("load", function() {
+  goToPage(1);
+  document.querySelector(".button-addtag").classList.add("active");
+});
+
 // Функция для перехода на указанную страницу
 function goToPage(pageNumber) {
   var pages = document.getElementsByClassName("page");
@@ -11,9 +17,16 @@ function goToPage(pageNumber) {
     pages[pageNumber - 1].style.transform = "translateY(0)";
     pages[pageNumber - 1].style.opacity = 1;
   }, 50);
-}
 
-// Отображение первой страницы при загрузке сайта
-window.addEventListener("load", function() {
-  goToPage(1);
-});
+  // Добавляем и удаляем класс "active" у кнопок в зависимости от открытой страницы
+  var addButton = document.querySelector(".button-addtag");
+  var minusButton = document.querySelector(".button-minustag");
+
+  if (pageNumber === 1) {
+    addButton.classList.add("active");
+    minusButton.classList.remove("active");
+  } else if (pageNumber === 2) {
+    addButton.classList.remove("active");
+    minusButton.classList.add("active");
+  }
+}
